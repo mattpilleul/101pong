cb61b8479e75e6896c22674b89d6429c31de8244
Matthieu Pilleul <matthieu.pilleul@epitech.eu>
2020-05-24 19:21:14 +0200

    Add include file

---
+++ include/my.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** Project
+** File description:
+** functions
+*/
+
+#ifndef	MY_H
+#define	MY_H
+
+#include <stddef.h>
+
+void my_putchar(char);
+void my_putstr(char *);
+int my_getnbr(char *);
+void man_disp(void);
+void disp_pong(float [], int n, float [], char **);
+int my_strlen(char *);
+int my_strcmp2(char *, char *);
+void my_check_f(char *);
+
+#endif
